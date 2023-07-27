73f4ea2c39f40a53db36cbecd8cba2773ec42206
davids1989 <daviddegusmao@gmail.com>
2023-07-27 14:11:15 -0300

    v1.0

---
+++ index.js
@@ -0,0 +1,41 @@
+const venom = require('venom-bot');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+venom
+  .create(
+    'sessionName',
+    (base64Qr, asciiQR) => {
+      console.log('Terminal QR code:\n', asciiQR);
+      // Opção para exibir o QRCode em algum lugar, como enviar por email, salvar em arquivo, etc.
+      // console.log('Base64 QR code image:\n', base64Qr);
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    console.log('WhatsApp Bot conectado com sucesso');
+
+    // Configurar as funcionalidades e manipulação de mensagens aqui
+
+    app.post('/send-message', (req, res) => {
+      const { number, message } = req.body;
+      client
+        .sendText(`${number}@c.us`, message)
+        .then(() => {
+          res.status(200).json({ success: true, message: 'Mensagem enviada com sucesso' });
+        })
+        .catch((error) => {
+          res.status(500).json({ success: false, message: 'Erro ao enviar mensagem', error });
+        });
+    });
+
+    app.listen(port, () => {
+      console.log(`API está rodando em http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.error('Erro ao iniciar o WhatsApp Bot', error));
